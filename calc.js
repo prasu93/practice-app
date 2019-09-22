932779df21ae8d7fce605b5f6a5453ca4879639f
prasu93 <prasannamahesh93@gmail.com>
2019-09-22 11:25:07 -0500

    edited js file

--- calc.js
+++ calc.js
@@ -3,8 +3,8 @@ function generateBill() {
     const rate=document.getElementById("rate").value;
     const tip=parseFloat(total)*rate/100;
     const tax= parseFloat(total)*5.5/100;
-    document.getElementById("tax").innerHTML="Tax: $"+tax;
+    document.getElementById("tax").innerHTML="Tax: $"+parseFloat(tax).toFixed(2);
     const grandtotal=parseFloat(total)+tax+parseFloat(tip);
-    document.getElementById("total").innerHTML="Grand Total: $"+grandtotal;
-    document.getElementById("tip").innerHTML="Tip: $"+tip;
+    document.getElementById("total").innerHTML="Grand Total: $"+parseFloat(grandtotal).toFixed(2);
+    document.getElementById("tip").innerHTML="Tip: $"+parseFloat(tip).toFixed(2);
 }
